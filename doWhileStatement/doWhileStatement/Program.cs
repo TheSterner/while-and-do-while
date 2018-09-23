a3bd14bdba2337f517cfd752d6bc6e128e476e55
William B McAllister <blakemcallister77@gmail.com>
2018-09-23 13:09:00 -0400

    a

---
+++ doWhileStatement/doWhileStatement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+
+                switch (number)
+                {
+                    case 48:
+                        Console.WriteLine("You guessed 48. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 34:
+                        Console.WriteLine("You guessed 34. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 14:
+                        Console.WriteLine("You guessed 14. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 24:
+                        Console.WriteLine("You guessed 24. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 59:
+                        Console.WriteLine("You guessed 59. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. You are correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ whileStatement/whileStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to play a game. Guess my favorite number. Enter your guess.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool guess = number == 14;
+
+            while (!guess)
+            {
+                switch (number)
+                {
+                    case 14:
+                        Console.WriteLine("You guessed 14. You are correct! Well Done!");
+                        guess = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You guessed incorrectly. Please try again.");
+                        Console.WriteLine("Your guess?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
